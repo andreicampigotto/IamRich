cd87d48abeeb3ffb0e2b31d0b3c48a7cb24a0054
Andrei Campigotto <andreicampigotto@gmail.com>
2022-09-11 11:12:15 -0300

    Initial Commit

---
+++ I am Rich/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I am Rich
+//
+//  Created by Andrei Campigotto on 9/11/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
